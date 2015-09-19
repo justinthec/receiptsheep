ae0fd5c82fdb514610f6e891fbcfdff4795dbefa
Justin Chan <justin.the.c@gmail.com>
2015-09-19 17:27:11 -0400

    Hooked up OCR script to upload

--- routes/image.js
+++ routes/image.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var fs = require('fs');
+var ocrscript = require('../lib/ocrscript');
 
 
 var upload = multer({
-    dest: './public/images/',
+    dest: './public/scans/',
     limits: {fileSize: 10000000, files:1}
 });
 
@@ -25,29 +26,31 @@ router.post('/', [upload, function(req, res, next) {
   var newFileName = (new Date().getTime())+"."+extension;
   console.log(newFileName);
 
-  fs.rename('./public/images/'+filename,
-    './public/images/'+ newFileName,
+  fs.renameSync('./public/scans/'+filename,
+    './public/scans/'+ newFileName/*,
     function(err) {
       if (err) {
         //handle
       }
       console.log("renamed "+filename+" to "+newFileName);
-  });
+  }*/);
 
   var dbjson = req.dbjson;
-  var resultJSON = {};
-  resultJSON.imageLocation = '/images/'+newFileName;
+  var expenseJSON = {};
+  expenseJSON.imageLocation = '/scans/'+newFileName;
+  expenseJSON.id = parseInt(newFileName, 10);
 
     //parser json file goes here
+    ocrscript.runOCR('./public/scans/'+newFileName, __dirname + '/../lib/temp.txt');
+    var file = fs.readFileSync(__dirname + '/../lib/temp.txt', 'utf8');
+    expenseJSON.text = file;
 
 
 
+  dbjson.push(expenseJSON); //Test
 
 
-  dbjson.push(resultJSON); //Test
-
-
-  res.send(JSON.stringify(resultJSON));
+  res.send(JSON.stringify(expenseJSON));
 }]);
 
 module.exports = router;
