e4c3529d464881d17622d257b2198cf93b1be30b
YOBrandon Chow <me@branchow.com>
2015-09-20 03:00:59 -0400

    Everything I Have So Far

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
 
   $("#approve").click(function(){
     $('.card').fadeOut( "slow", function() {
+      location.reload(true);
     });        
   });  
 
   $("#decline").click(function(){
     $('.card').fadeOut( "slow", function() {
+      location.reload(true);
     });
   });
 });
+
+/* Inserting Cards */
